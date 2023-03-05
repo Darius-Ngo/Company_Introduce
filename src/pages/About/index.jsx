4036883c97b42637194d1a71b9ea583dc6deb4e9
LongNV <vlongit222@gmail.com>
2023-03-05 17:15:28 +0700

    update

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -4,27 +4,27 @@ import { WrapAboutPage } from "./styled";
 const AboutPage = () => {
   return (
     <div className="">
-      <WrapAboutPage>
-        <WrapHeader>
-          <div className="slogan d-flex align-items-center justify-content-center">
-            <h2>Warm heart - Warms hands</h2>
-          </div>
-        </WrapHeader>
+      <WrapHeader>
+        <div className="slogan d-flex align-items-center justify-content-center">
+          <h2>Warm heart - Warms hands</h2>
+        </div>
+      </WrapHeader>
+      <WrapAboutPage className="page-common">
         <div className="title-main">Heart and Virtue</div>
         <div className="subtitle">Warm heart- Warm hands</div>
-        <p>
+        <div className="content">
           We focus on empowering individuals, encouraging them to develop skills
           for daily life and healthy relationships, as well as supporting them
           to boost their confidence every day.
-        </p>
+        </div>
         <div className="subtitle">Our vision </div>
-        <p>
+        <div className="content">
           We put our hearts, our professional and conscientious care in a
           generation to ensure people live happier, healthier and more
           meaningful lives.
-        </p>
+        </div>
         <div className="subtitle">Who are Heart and Virtue together?</div>
-        <p>
+        <div className="content">
           We have a strong expertise in supporting people with particularly
           complex needs, including autism, behaviours which may challenge mental
           health conditions and associated complex health needs. We ensure that
@@ -32,7 +32,7 @@ const AboutPage = () => {
           forward to working with you and your teams over the coming months.
           Together, we can continue to exceed expectations and make a real
           difference to the lives of people we support.
-        </p>
+        </div>
       </WrapAboutPage>
     </div>
   );
